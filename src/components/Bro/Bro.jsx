00422c3064df87549e97acbdcb73183c1e57f001
tSult <sultanatasmia61@gmail.com>
2025-02-12 22:42:44 +0600

    Context API to avoid Prop drilling

--- src/components/Bro/Bro.jsx
+++ src/components/Bro/Bro.jsx
@@ -1,9 +1,13 @@
+import { useCallback, useContext } from "react";
+import { MoneyContext } from "../Grandpa/Grandpa";
 
 
 const Bro = () => {
+    const []=useContext(MoneyContext);
     return (
         <div>
             <h2>Brother</h2>
+            <p>Grandpa:{money}</p>
         </div>
     );
 };
--- src/components/Grandpa/Grandpa.jsx
+++ src/components/Grandpa/Grandpa.jsx
@@ -1,19 +1,28 @@
 import Dad from "../Dad/Dad";
 import Uncle from "../Uncle/Uncle";
 import Aunty from "../Aunty/Aunty";
-import './Grandpa.css';
+import "./Grandpa.css";
+import { createContext, useState } from "react";
+export const AssetContext = createContext("gold");
+export const MoneyContext=createContext(1000);
 const Grandpa = () => {
-    const asset='diamond';
-    return (
-        <div className="grandpa">
-            <h2>Grandpa</h2>
-            <section className=" flex">
-               <Dad asset={asset}></Dad>
-               <Uncle asset={asset}></Uncle>
-               <Aunty></Aunty>
-            </section>
-        </div>
-    );
+    const[money,setMoney] =useState(1000);
+  const asset = "diamond";
+  return (
+    <div className="grandpa">
+        <p>Net money:{money}</p>
+      <h2>Grandpa</h2>
+      <MoneyContext.Provider value={[money,setMoney]}>
+      <AssetContext.Provider value="gold">
+        <section className=" flex">
+          <Dad asset={asset}></Dad>
+          <Uncle asset={asset}></Uncle>
+          <Aunty></Aunty>
+        </section>
+      </AssetContext.Provider>
+      </MoneyContext.Provider>
+    </div>
+  );
 };
 
 export default Grandpa;
